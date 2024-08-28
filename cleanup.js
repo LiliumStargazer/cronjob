d0a7baca3980b020eddca38ebf1ad1bfd1862f96
LiliumStargazer <129180301+LiliumStargazer@users.noreply.github.com>
2024-08-28 10:21:34 +0200

    fixed missing script, added dockerfile

--- cleanup.js
+++ cleanup.js
@@ -3,7 +3,6 @@ import * as Sentry from '@sentry/node';
 import http from 'http';
 import cron from 'node-cron';
 
-
 // Inizializza Sentry per il monitoraggio degli errori
 Sentry.init({
     dsn: "https://3df2459d1349ceba4313c89c97860ad6@o4507295315525632.ingest.de.sentry.io/4507295325028432",
@@ -11,7 +10,6 @@ Sentry.init({
     debug: false,
 });
 
-// Funzione per eseguire la pulizia
 function runCleanup() {
     const options = {
         hostname: 'amservice.ampiovani.locale',
@@ -19,7 +17,6 @@ function runCleanup() {
         path: '/api/backups/cleanup',
         method: 'DELETE'
     };
-
     const req = http.request(options, res => {
         if (res.statusCode === 200) {
             console.log("Cleanup completed successfully");
@@ -27,20 +24,17 @@ function runCleanup() {
             console.log(`Failed to cleanup: ${res.statusCode}`);
         }
     });
-
     req.on('error', error => {
         console.error(error);
         Sentry.captureException(error);
     });
-
     req.end();
 }
 
-// Programmare il cron job per eseguire la pulizia ogni notte a mezzanotte
+//pulizia ogni notte a mezzanotte
 cron.schedule('0 0 * * *', () => {
     console.log('Running cleanup task at midnight');
     runCleanup();
 });
 
-// Per mantenere il  container in esecuzione
 console.log('Cron job scheduled. Container is running...');
